fb36b664fb4d6994e65b5af6bd25823580f2e2f6
Furkan Bozdağ <bozd4g@hotmail.com>
2018-10-06 15:45:02 +0300

    Add .babelrc for babel plugin. Remove defaulf files source. Edit index.js for project. Add MainPage.jsx and their folder for components. Add style.css and their folder for whole styles.

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/styles.css';
+
+export default class Mainpage extends React.Component {
+    render() {
+        return(
+            <div className='main'>hello world!</div>
+        );
+    };
+}
